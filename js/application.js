1522f6ab80e9f63b2b80d685b4a7127bd88ccd46
maxwellworkman <maxwell.workman@gmail.com>
2016-12-13 15:52:53 -0800

    add string lightness changer

--- js/application.js
+++ js/application.js
@@ -1,26 +1,38 @@
-var colorPalette = {
+function hslLChange(hslString, newLVal) {
+	return hslString.replace(/...\)$/g, newLVal + "%)")
+}
+
+var colorPalettes = {
 	morning: { sky: "", beach: "", ocean: "", orb: "" },
 	daytime: { sky: "", beach: "", ocean: "", orb: "" },
 	evening: { sky: "", beach: "", ocean: "", orb: "" },
-	night: { sky: "hsl(219, 80%, 14%)", lightSky: "hsl(219, 80%, 30%)",beach: "", ocean: "", orb: "",  }
+	night: { 
+		sky: "hsl(219, 80%, 11%)", 
+		lightSky: "hsl(219, 80%, 30%)",
+		beach: "", 
+		ocean: "hsl(219, 80%, 15%)", 
+		lightOcean: "hsl(219, 80%, 29%)",
+		orb: "",  
+	}
 }
 
 var time = "night";
 
 switch(time) {
 	case "morning":
-		var palette = colorPalette.morning;
+		var palette = colorPalettes.morning;
 		break;
 	case "daytime":
-		var palette = colorPalette.daytime;
+		var palette = colorPalettes.daytime;
 		break;
 	case "evening":
-		var palette = colorPalette.evening;
+		var palette = colorPalettes.evening;
 		break;
 	case "night":
-		var palette = colorPalette.night;
+		var palette = colorPalettes.night;
 		break;
 }
+
 
 var c
 var ctx,
@@ -43,8 +55,14 @@ Beach = {
 		ctx.fillRect(0,0,ctx.canvas.width,ctx.canvas.height/2);
   	},
   	drawOcean: function() {
-  		var grd=ctx.createLinearGradient();
+  		var grd=ctx.createLinearGradient(ctx.canvas.width/2,(ctx.canvas.height/2)-1,ctx.canvas.width/2,ctx.canvas.height);
+  		grd.addColorStop(0,palette.lightOcean);
+  		grd.addColorStop(1,palette.ocean);
+  		ctx.fillStyle=grd;
   		ctx.fillRect(0,(ctx.canvas.height/2)-1,ctx.canvas.width,ctx.canvas.height)
+  	},
+  	drawReflections: function() {
+
   	}
 }
 
